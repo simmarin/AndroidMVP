9d6d38c3766571f7dec41046687ef6a08e883a0c
simmarin <s.mtz.marin@gmail.com>
2017-09-26 19:56:37 -0700

    add dagger and class

---
+++ app/src/androidTest/java/com/example/clon/androidmvp/model/Product.java
@@ -0,0 +1,7 @@
+package com.example.clon.androidmvp.model;
+
+/**
+ * Created by CLON on 26/09/2017.
+ */
+public class Product {
+}
---
+++ app/src/main/java/com/example/clon/androidmvp/dagger/ShoppingCartModule.java
@@ -0,0 +1,7 @@
+package com.example.clon.androidmvp.dagger;
+
+/**
+ * Created by CLON on 26/09/2017.
+ */
+public class ShoppingCartModule {
+}
